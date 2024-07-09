ed5e06894182d1983c5c1f73f5645750c05b36bc
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-07-09 20:18:05 +0300

    .

--- lib/views/Widget/all_expensses/all_expensses_item_list_view.dart
+++ lib/views/Widget/all_expensses/all_expensses_item_list_view.dart
@@ -3,7 +3,7 @@ import 'package:responsive_dashboard/models/all_expensses_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
 import 'package:responsive_dashboard/views/Widget/all_expensses/all_expensses_item.dart';
 
-class AllExpenssesItemListView extends StatelessWidget {
+class AllExpenssesItemListView extends StatefulWidget {
   const AllExpenssesItemListView({super.key});
   static List<AllExpenssesItemModel> itemModel = [
     AllExpenssesItemModel(
@@ -24,30 +24,57 @@ class AllExpenssesItemListView extends StatelessWidget {
   ];
 
   @override
+  State<AllExpenssesItemListView> createState() =>
+      _AllExpenssesItemListViewState();
+}
+
+class _AllExpenssesItemListViewState extends State<AllExpenssesItemListView> {
+  int activeIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Row(
-        children: itemModel.asMap().entries.map((e) {
+        children: AllExpenssesItemListView.itemModel.asMap().entries.map((e) {
       if (e.key == 1) {
         return Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: AllExpenssesItem(
-              allExpenssesItemModel: e.value,
-              isSelected: false,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                updateIndex(activeIndex);
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: AllExpenssesItem(
+                allExpenssesItemModel: e.value,
+                isSelected: activeIndex == e.key ? true : false,
+              ),
             ),
           ),
         );
       } else {
         return Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(0),
-            child: AllExpenssesItem(
-              allExpenssesItemModel: e.value,
-              isSelected: true,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                updateIndex(activeIndex);
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: AllExpenssesItem(
+                allExpenssesItemModel: e.value,
+                isSelected: activeIndex == e.key ? true : false,
+              ),
             ),
           ),
         );
       }
     }).toList());
+  }
+
+  void updateIndex(int activeIndex) {
+    setState(() {
+      activeIndex = activeIndex;
+    });
   }
 }
