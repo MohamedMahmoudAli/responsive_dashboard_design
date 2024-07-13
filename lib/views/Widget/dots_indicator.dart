162ab3c9ebb2cf13c1354345d4ecea8625b74990
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-07-13 17:22:52 +0300

    finish my card section

--- lib/views/Widget/dots_indicator.dart
+++ lib/views/Widget/dots_indicator.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/Widget/custom_dot_indicator.dart';
 
 class DotsIndicator extends StatelessWidget {
-  const DotsIndicator({super.key});
+  const DotsIndicator({super.key, required this.activeIndex});
+  final int activeIndex;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: List.generate(
           3,
-          (index) => const Padding(
-                padding: EdgeInsets.only(right: 8),
-                child: CustomDotIndicator(isActive: true),
+          (index) =>  Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: CustomDotIndicator(isActive:  index== activeIndex),
               )),
     );
   }
--- lib/views/Widget/my_cards_section.dart
+++ lib/views/Widget/my_cards_section.dart
@@ -1,28 +1,57 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/utils/app_style.dart';
 import 'package:responsive_dashboard/views/Widget/dots_indicator.dart';
 import 'package:responsive_dashboard/views/Widget/my_cards_page_view.dart';
 
-class MyCards extends StatelessWidget {
+class MyCards extends StatefulWidget {
   const MyCards({super.key});
+
+  @override
+  State<MyCards> createState() => _MyCardsState();
+}
+
+class _MyCardsState extends State<MyCards> {
+  late PageController pageController;
+  late int currentIndex = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+
+    pageController.addListener(() {
+      currentIndex = pageController.page!.round();
+      log(currentIndex.toString());
+      setState(() {});
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          "MY Card",
-          style: AppStyles.styleSemiBold20(context),
+        SizedBox(
+          width: 420,
+          child: Text(
+            "MY Card",
+            style: AppStyles.styleSemiBold20(context),
+          ),
         ),
         const SizedBox(
           height: 20,
         ),
-        const MyCardsPageView(),
+        MyCardsPageView(
+          pageController: pageController,
+        ),
         const SizedBox(
           height: 20,
         ),
-        const DotsIndicator()
+        DotsIndicator(
+          activeIndex: currentIndex,
+        )
       ],
     );
   }
